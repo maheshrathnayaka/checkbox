deb5b31e37e4da9c1029eafe970d86315ede8839
maheshrathnayaka <mintcrops@gmail.com>
2015-07-15 01:06:56 +0530

    add checkbox selection example

---
+++ src/checkbox/Checkbox.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package checkbox;
+
+/**
+ *
+ * @author mahesh
+ */
+public class Checkbox {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Selection s = new Selection();
+        s.setVisible(true);
+    }
+    
+}
